479acaad626f226da908029d325d496dae4ea1cf
TokenToucan <tokentoucan@gmail.com>
2016-03-10 23:03:24 -0500

    Implements 'Find', 'Undo', and 'Redo' commands

--- WhacmeCommands.py
+++ WhacmeCommands.py
@@ -121,6 +121,56 @@ class WhacmeCommands():
 			rChar = self.owner.tag.text.get('1.%s' % right, '1.%s' % (right + 1))
 		self.owner.tag.text.delete('1.0', '1.%s' % right)
 		self.owner.tag.text.insert('1.0', self.owner.path)
+	
+	def save(self):
+		# get the path in the tag - this is if you want to change the filename before saving
+		lEnd = int(self.owner.tag.text.index('1.0 lineend').split('.')[1])
+		right = 1
+		rChar = self.owner.tag.text.get('1.0', '1.1')
+		while (right <= lEnd) and (not rChar.isspace()):
+			right += 1
+			rChar = self.owner.tag.text.get('1.%s' % right, '1.%s' % (right + 1))
+		tagPath = os.path.realpath(self.owner.tag.text.get('1.0', '1.%s' % right))
+		
+		outFile = open(tagPath, 'w')
+		errFile = open(tagPath + '+err', 'w')
+		
+		outFile.write(u'%s' % self.owner.mainText.text.get('1.0', 'end'))
+		errFile.write(u'%s' % self.owner.sideText.text.get('1.0', 'end'))
+		
+		self.owner.tag.indicator.configure(bg=self.owner.tag.darkColor)
+
+	def undo(self):
+		self.owner.mainText.text.edit_undo()
+
+	def redo(self):
+		self.owner.mainText.text.edit_redo()
+	
+	def find(self, args):
+		# remove the initial 'Find' keyword to get the search pattern
+		pattern = reduce(lambda x,y: x + y, args[1:], '')
+		
+		start = '1.0'
+		cnt = tk.IntVar()
+		
+		# loop through the text and mark what needs to be highlighted
+		# TODO: implement scrollbar marking
+		while True:
+			lIndex = self.owner.mainText.text.search(pattern, start, stopindex='end', count=cnt)
+			rIndex = '%s+%sc' % (lIndex, cnt.get())
+
+			# exit if we do not find anything
+			if lIndex == '':
+				break
+		
+			# set highlight tag on the found text
+			self.owner.mainText.text.tag_add('highlightText', lIndex, rIndex)
+
+			# set the new start location
+			start = rIndex
+		
+		# set the highlighting on
+		self.owner.mainText.text.tag_configure('highlightText', background=self.owner.mainText.highlightColor)
 
 	def runCommand(self, cmd):
 		splitCmd = cmd.split(' ')
@@ -146,24 +196,6 @@ class WhacmeCommands():
 		else:
 			self.owner.sideText.text.insert('1.0', '###################\n')
 			self.owner.sideText.text.insert('1.0', subprocess.check_output('winid=' + self.owner.id + ' && ' + cmd, shell=True))
-	
-	def save(self):
-		# get the path in the tag - this is if you want to change the filename before saving
-		lEnd = int(self.owner.tag.text.index('1.0 lineend').split('.')[1])
-		right = 1
-		rChar = self.owner.tag.text.get('1.0', '1.1')
-		while (right <= lEnd) and (not rChar.isspace()):
-			right += 1
-			rChar = self.owner.tag.text.get('1.%s' % right, '1.%s' % (right + 1))
-		tagPath = os.path.realpath(self.owner.tag.text.get('1.0', '1.%s' % right))
-		
-		outFile = open(tagPath, 'w')
-		errFile = open(tagPath + '+err', 'w')
-		
-		outFile.write(u'%s' % self.owner.mainText.text.get('1.0', 'end'))
-		errFile.write(u'%s' % self.owner.sideText.text.get('1.0', 'end'))
-		
-		self.owner.tag.indicator.configure(bg=self.owner.tag.darkColor)
 
 
 			
